824ef778fce69389cc1f3917084ea01a1b4f0b3d
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-01-19 12:30:40 +0200

    fixed the local storage bug

--- JS/results.js
+++ JS/results.js
@@ -1,6 +1,87 @@
 'use strict';
 
-const products = JSON.parse(localStorage.getItem('products'));
+const products = JSON.parse(localStorage.getItem('products')) || [
+	{ id: 1, name: 'bag', src: 'img/bag.jpg', shownTimes: 0, clicked: 0 },
+	{ id: 2, name: 'banana', src: 'img/banana.jpg', shownTimes: 0, clicked: 0 },
+	{
+		id: 3,
+		name: 'bathroom',
+		src: 'img/bathroom.jpg',
+		shownTimes: 0,
+		clicked: 0,
+	},
+	{ id: 4, name: 'boots', src: 'img/boots.jpg', shownTimes: 0, clicked: 0 },
+	{
+		id: 5,
+		name: 'breakfast',
+		src: 'img/breakfast.jpg',
+		shownTimes: 0,
+		clicked: 0,
+	},
+	{
+		id: 6,
+		name: 'bubblegum',
+		src: 'img/bubblegum.jpg',
+		shownTimes: 0,
+		clicked: 0,
+	},
+	{ id: 7, name: 'chair', src: 'img/chair.jpg', shownTimes: 0, clicked: 0 },
+	{ id: 8, name: 'cthulhu', src: 'img/cthulhu.jpg', shownTimes: 0, clicked: 0 },
+	{
+		id: 9,
+		name: 'dog-duck',
+		src: 'img/dog-duck.jpg',
+		shownTimes: 0,
+		clicked: 0,
+	},
+	{ id: 10, name: 'dragon', src: 'img/dragon.jpg', shownTimes: 0, clicked: 0 },
+	{ id: 11, name: 'pen', src: 'img/pen.jpg', shownTimes: 0, clicked: 0 },
+	{
+		id: 12,
+		name: 'pet-sweep',
+		src: 'img/pet-sweep.jpg',
+		shownTimes: 0,
+		clicked: 0,
+	},
+	{
+		id: 13,
+		name: 'scissors',
+		src: 'img/scissors.jpg',
+		shownTimes: 0,
+		clicked: 0,
+	},
+	{ id: 14, name: 'shark', src: 'img/shark.jpg', shownTimes: 0, clicked: 0 },
+	{ id: 15, name: 'sweep', src: 'img/sweep.png', shownTimes: 0, clicked: 0 },
+	{
+		id: 16,
+		name: 'tauntaun',
+		src: 'img/tauntaun.jpg',
+		shownTimes: 0,
+		clicked: 0,
+	},
+	{
+		id: 17,
+		name: 'unicorn',
+		src: 'img/unicorn.jpg',
+		shownTimes: 0,
+		clicked: 0,
+	},
+	{ id: 18, name: 'usb', src: 'img/usb.gif', shownTimes: 0, clicked: 0 },
+	{
+		id: 19,
+		name: 'water-can',
+		src: 'img/water-can.jpg',
+		shownTimes: 0,
+		clicked: 0,
+	},
+	{
+		id: 20,
+		name: 'wine-glass',
+		src: 'img/wine-glass.jpg',
+		shownTimes: 0,
+		clicked: 0,
+	},
+];
 
 const productsName = [];
 const views = [];
@@ -22,14 +103,10 @@ const listItem = () => {
 };
 
 const imageShown = () => {
-	for (
-		let index = 0;
-		index < JSON.parse(localStorage.getItem('products')).length;
-		index++
-	) {
-		productsName.push(JSON.parse(localStorage.getItem('products'))[index].name);
-		views.push(JSON.parse(localStorage.getItem('products'))[index].shownTimes);
-		votes.push(JSON.parse(localStorage.getItem('products'))[index].clicked);
+	for (let index = 0; index < products.length; index++) {
+		productsName.push(products[index].name);
+		views.push(products[index].shownTimes);
+		votes.push(products[index].clicked);
 	}
 
 	let productChart = new Chart(chart, {
@@ -63,4 +140,3 @@ window.addEventListener('DOMContentLoaded', () => {
 	imageShown();
 	listItem();
 });
-
