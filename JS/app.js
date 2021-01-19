73d1919c02e0c61ea12f6c501cce5cfa0b62a338
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-01-19 13:48:02 +0200

    made the constructor function

--- JS/app.js
+++ JS/app.js
@@ -1,87 +1,94 @@
 'use strict';
 
-const products = [
-	{ id: 1, name: 'bag', src: 'img/bag.jpg', shownTimes: 0, clicked: 0 },
-	{ id: 2, name: 'banana', src: 'img/banana.jpg', shownTimes: 0, clicked: 0 },
-	{
-		id: 3,
-		name: 'bathroom',
-		src: 'img/bathroom.jpg',
-		shownTimes: 0,
-		clicked: 0,
-	},
-	{ id: 4, name: 'boots', src: 'img/boots.jpg', shownTimes: 0, clicked: 0 },
-	{
-		id: 5,
-		name: 'breakfast',
-		src: 'img/breakfast.jpg',
-		shownTimes: 0,
-		clicked: 0,
-	},
-	{
-		id: 6,
-		name: 'bubblegum',
-		src: 'img/bubblegum.jpg',
-		shownTimes: 0,
-		clicked: 0,
-	},
-	{ id: 7, name: 'chair', src: 'img/chair.jpg', shownTimes: 0, clicked: 0 },
-	{ id: 8, name: 'cthulhu', src: 'img/cthulhu.jpg', shownTimes: 0, clicked: 0 },
-	{
-		id: 9,
-		name: 'dog-duck',
-		src: 'img/dog-duck.jpg',
-		shownTimes: 0,
-		clicked: 0,
-	},
-	{ id: 10, name: 'dragon', src: 'img/dragon.jpg', shownTimes: 0, clicked: 0 },
-	{ id: 11, name: 'pen', src: 'img/pen.jpg', shownTimes: 0, clicked: 0 },
-	{
-		id: 12,
-		name: 'pet-sweep',
-		src: 'img/pet-sweep.jpg',
-		shownTimes: 0,
-		clicked: 0,
-	},
-	{
-		id: 13,
-		name: 'scissors',
-		src: 'img/scissors.jpg',
-		shownTimes: 0,
-		clicked: 0,
-	},
-	{ id: 14, name: 'shark', src: 'img/shark.jpg', shownTimes: 0, clicked: 0 },
-	{ id: 15, name: 'sweep', src: 'img/sweep.png', shownTimes: 0, clicked: 0 },
-	{
-		id: 16,
-		name: 'tauntaun',
-		src: 'img/tauntaun.jpg',
-		shownTimes: 0,
-		clicked: 0,
-	},
-	{
-		id: 17,
-		name: 'unicorn',
-		src: 'img/unicorn.jpg',
-		shownTimes: 0,
-		clicked: 0,
-	},
-	{ id: 18, name: 'usb', src: 'img/usb.gif', shownTimes: 0, clicked: 0 },
-	{
-		id: 19,
-		name: 'water-can',
-		src: 'img/water-can.jpg',
-		shownTimes: 0,
-		clicked: 0,
-	},
-	{
-		id: 20,
-		name: 'wine-glass',
-		src: 'img/wine-glass.jpg',
-		shownTimes: 0,
-		clicked: 0,
-	},
+const imgId = [
+	1,
+	2,
+	3,
+	4,
+	5,
+	6,
+	7,
+	8,
+	9,
+	10,
+	11,
+	12,
+	13,
+	14,
+	15,
+	16,
+	17,
+	18,
+	19,
+	20,
 ];
+const imgName = [
+	'bag',
+	'banana',
+	'bathroom',
+	'boots',
+	'breakfast',
+	'bubblegum',
+	'chair',
+	'cthulhu',
+	'dog-duck',
+	'dragon',
+	'pen',
+	'pet-sweep',
+	'scissors',
+	'shark',
+	'sweep',
+	'tauntaun',
+	'unicorn',
+	'usb',
+	'water-can',
+	'wine-glass',
+];
+const imgSrc = [
+	'img/bag.jpg',
+	'img/banana.jpg',
+	'img/bathroom.jpg',
+	'img/boots.jpg',
+	'img/breakfast.jpg',
+	'img/bubblegum.jpg',
+	'img/chair.jpg',
+	'img/cthulhu.jpg',
+	'img/dog-duck.jpg',
+	'img/dragon.jpg',
+	'img/pen.jpg',
+	'img/pet-sweep.jpg',
+	'img/scissors.jpg',
+	'img/shark.jpg',
+	'img/sweep.png',
+	'img/tauntaun.jpg',
+	'img/unicorn.jpg',
+	'img/usb.gif',
+	'img/water-can.jpg',
+	'img/wine-glass.jpg',
+];
+const imgViews = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+const imgVotes = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+const products = [];
+
+function Product(id, name, src, shownTimes, clicked) {
+	this.id = id;
+	this.name = name;
+	this.src = src;
+	this.shownTimes = shownTimes;
+	this.clicked = clicked;
+}
+
+for (let i = 0; i < 20; i++) {
+	const product = new Product(
+		imgId[i],
+		imgName[i],
+		imgSrc[i],
+		imgViews[i],
+		imgVotes[i],
+	);
+	products.push(product);
+}
 
 const productsName = [];
 const views = [];
